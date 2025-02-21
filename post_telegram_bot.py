fb6b6aa9522730725ad26df4b271f693cd7bd3be
VASILIYKAS <yoyoynv@gmail.com>
2025-02-21 15:28:10 +0500

    Добавлена проверка существования файла для внешнего кода

--- post_telegram_bot.py
+++ post_telegram_bot.py
@@ -1,14 +1,12 @@
+import os
 
 
 def post_image(bot, channel_id, comics_title):
-    if not comics_title:
-        return 'No images found in the directory.'
+    if not comics_title or not os.path.exists(comics_title):
+        raise FileNotFoundError(f'Комикс с именем {comics_title} не найден.')
 
-    if comics_title:
-        send_commics(bot, channel_id, comics_title)
-    else:
-        return 'The image name is incorrect. Such an image was not found.'
-    
+    send_commics(bot, channel_id, comics_title)
+
 
 def send_commics(bot, channel_id, comics_title):
     with open(comics_title, 'rb') as photo:
